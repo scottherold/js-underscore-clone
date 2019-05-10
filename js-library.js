e27ddb8ed2a9bfcd26350deacd70435fccda8365
Scott Herold <41196046+scottherold@users.noreply.github.com>
2019-05-10 15:10:53 -0400

    Add files via upload
    Removed testing language.

--- js-library.js
+++ js-library.js
@@ -46,7 +46,3 @@ var _ = {
         return newArr;
     }
 }
-// you just created a library with 5 methods!
-
-var odds = _.reject([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
-console.log(odds); // if things are working right, this will return [1,3,5].
